ff88e2a14c54f55365153321fa0cf1699724ff85
jdbdev <juliendb.dev@gmail.com>
2025-04-05 17:27:59 -0300

    func main() implemented with .env file

--- main.go
+++ main.go
@@ -2,3 +2,26 @@ package main
 
 // Simple app connects to openweathermap.org API
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+func main(){
+// Load .env file:
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("error loading .env file")
+	}
+// Get API key: 
+apiKey := os.Getenv("API_KEY")
+if apiKey == ""{
+	log.Fatal("Error: API_KEY has no assigned value")
+}
+// Print API key:
+fmt.Println("Your API Key is:", apiKey)
+}

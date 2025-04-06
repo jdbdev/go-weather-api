a70d95d08e8865ff7ebf59408532f76c4d255af5
jdbdev <juliendb.dev@gmail.com>
2025-04-06 19:53:26 -0300

    HTTP request with sample API url working, status 401

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-// Simple app connects to openweathermap.org API
+// Simple app connects to openweathermap.org API. Abbreviated to owm in app.
 
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -12,6 +13,9 @@ import (
 
 
 func main(){
+
+// 01 ENV SETUP: 
+
 // Load .env file:
 	err := godotenv.Load()
 	if err != nil{
@@ -24,4 +28,36 @@ if apiKey == ""{
 }
 // Print API key:
 fmt.Println("Your API Key is:", apiKey)
+
+// Prompt user for city name: 
+var city string 
+fmt.Println("enter name of city:")
+fmt.Scanln(&city)
+fmt.Println("Your city is:", city)
+
+// 02 BUILD REQUEST URL:
+
+// Sample API Call from owm:
+urlDemo:= fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=%s", apiKey)
+fmt.Println(urlDemo)
+
+// // Build the request URL
+// url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric&lang=fr", city, apiKey)
+// fmt.Println(url)
+
+// Send HTTP request: 
+response, err := http.Get(urlDemo)
+	if err != nil{
+		log.Fatal("Error sending request to www.openweathermap.org")
+		fmt.Println()
+	}
+	defer response.Body.Close()
+	if response.StatusCode == 200 {
+		fmt.Println("HTTP status code 200")
+	}
+	
+	fmt.Println(response.Header)
+	fmt.Println("The HTTP status code is:", response.StatusCode)
+	fmt.Println("The HTTP Body is:", response.Body)
+
 }

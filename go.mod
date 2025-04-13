32b040bbd5e5afbffe1c5189e6a2e4947222fea6
jdbdev <juliendb.dev@gmail.com>
2025-04-13 13:44:00 -0300

    breaking down procedures into functions outside of main

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/jdbdev/go-weather-api
 
 go 1.23.5
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 // Simple app connects to openweathermap.org API. Abbreviated to owm in app.
+// Requires API key for One Call API 3.0 (include in .env file as API_KEY)
 
 import (
 	"fmt"
@@ -15,14 +16,16 @@ import (
 
 type weatherData struct{
 	Name string
+	City []location
+}
+type location struct{
+	City string
 }
 
 func main(){
 
-// 01 ENV SETUP: 
-
 // Load .env file:
-	err := godotenv.Load()
+err := godotenv.Load()
 	if err != nil{
 		log.Fatal("error loading .env file")
 	}
@@ -31,8 +34,6 @@ apiKey := os.Getenv("API_KEY")
 if apiKey == ""{
 	log.Fatal("Error: API_KEY has no assigned value")
 }
-// Print API key:
-fmt.Println("Your API Key is:", apiKey)
 
 // Prompt user for city name: 
 var city string 
@@ -40,26 +41,22 @@ fmt.Println("enter name of city:")
 fmt.Scanln(&city)
 fmt.Println("Your city is:", city)
 
-// 02 BUILD REQUEST URL:
+// ---------- 02 BUILD REQUEST URL ---------- //
 
 // Sample API Call from owm:
 urlDemo:= fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=%s", apiKey)
 fmt.Println(urlDemo)
 
-// // Build the request URL
-// url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric&lang=fr", city, apiKey)
-// fmt.Println(url)
+// ---------- 03 HTTP REQUEST AND RESPONSE HANDLING ---------- //
 
-// Send HTTP request: 
 response, err := http.Get(urlDemo)
 	if err != nil{
 		log.Fatal("Error sending request to www.openweathermap.org")
-		fmt.Println()
 	}
 	defer response.Body.Close()
-	if response.StatusCode != 200 {
-		log.Fatal("Something went wrong!")
-	}
+	// if response.StatusCode != 200 {
+	// 	log.Fatal("Something went wrong!")
+	// }
 	fmt.Println("The HTTP status code is:", response.StatusCode)
 	
 	body, err:= io.ReadAll(response.Body)
@@ -67,4 +64,25 @@ response, err := http.Get(urlDemo)
 		log.Fatal("there is no response body")
 	}
 	fmt.Println(string(body))
+}
+
+// Functions called from main{}
+func getCity (x string) (string, error){
+	return "", nil
+}
+func buildUrl (city,url string) (string, error){
+	return "", nil
+}
+func getWeather (api_key,url string) (string, error){
+	response, err := http.Get(url)
+	if err != nil{
+		log.Fatal("error")
+	}
+	defer response.Body.Close()
+	body, err:= io.ReadAll(response.Body)
+	if err != nil{
+		log.Fatal("there is no response body")
+	}
+
+	return string(body), nil
 }

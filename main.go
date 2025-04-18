d87782f74acb9d5007468e193db13b51db6ed9a5
jdbdev <juliendb.dev@gmail.com>
2025-04-18 11:48:07 -0300

    functions called from main all working and updating global variables

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-// Simple app connects to openweathermap.org API. Abbreviated to owm in app.
+// Simple app connects to openweathermap.org API.
 // Requires API key for One Call API 3.0 (include in .env file as API_KEY)
 
 import (
@@ -13,68 +13,37 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Global variables
+var apiKey string = ""
+var city string = ""
+var url string = ""
 
-type weatherData struct{
-	Name string
-	City []location
-}
-type location struct{
-	City string
-}
-
-func main(){
-
-// Load .env file:
-err := godotenv.Load()
+// Load .env variables and check for API key:
+func loadEnv () (string, error){
+	err := godotenv.Load()
 	if err != nil{
 		log.Fatal("error loading .env file")
 	}
-// Get API key: 
-apiKey := os.Getenv("API_KEY")
-if apiKey == ""{
-	log.Fatal("Error: API_KEY has no assigned value")
-}
-
-// Prompt user for city name: 
-var city string 
-fmt.Println("enter name of city:")
-fmt.Scanln(&city)
-fmt.Println("Your city is:", city)
-
-// ---------- 02 BUILD REQUEST URL ---------- //
-
-// Sample API Call from owm:
-urlDemo:= fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=%s", apiKey)
-fmt.Println(urlDemo)
-
-// ---------- 03 HTTP REQUEST AND RESPONSE HANDLING ---------- //
-
-response, err := http.Get(urlDemo)
-	if err != nil{
-		log.Fatal("Error sending request to www.openweathermap.org")
+	apiKey:= os.Getenv("API_KEY")
+	if apiKey == ""{
+		log.Fatal("Error: API_KEY has no assigned value")
 	}
-	defer response.Body.Close()
-	// if response.StatusCode != 200 {
-	// 	log.Fatal("Something went wrong!")
-	// }
-	fmt.Println("The HTTP status code is:", response.StatusCode)
-	
-	body, err:= io.ReadAll(response.Body)
-	if err != nil{
-		log.Fatal("there is no response body")
-	}
-	fmt.Println(string(body))
+	return "success loading .env file!", nil
 }
-
-// Functions called from main{}
-func getCity (x string) (string, error){
-	return "", nil
+// Function prompts user for city name and updates value at variable city:
+func updateCity(str *string){
+	var userCity string
+	fmt.Println("Enter your city name:")
+	fmt.Scanln(&userCity) 
+	*str = userCity
 }
-func buildUrl (city,url string) (string, error){
-	return "", nil
+// Build URL
+func buildURL(city, key string) string {
+	return ""
 }
-func getWeather (api_key,url string) (string, error){
-	response, err := http.Get(url)
+// Send HTTP Request to openweathermap.org
+func sendRequest (x string) (string, error){
+	response, err := http.Get(x)
 	if err != nil{
 		log.Fatal("error")
 	}
@@ -83,6 +52,26 @@ func getWeather (api_key,url string) (string, error){
 	if err != nil{
 		log.Fatal("there is no response body")
 	}
-
 	return string(body), nil
+}
+
+
+func main(){
+	// load .env file and check for API key:
+	env, err := loadEnv()
+	fmt.Println(env, err)
+	apiKey = os.Getenv("API_KEY")
+		if apiKey == ""{
+			log.Fatal("Error: API_KEY has no assigned value")
+		}
+	// Prompt user for city: 
+	updateCity(&city) 
+	fmt.Printf("The weather in %s is:", city)
+
+	// Build request URL: 
+	url = fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=%s", apiKey)
+
+	// send HTTP Request to openweathermap.org:
+	responseBody, _ := sendRequest(url)
+	fmt.Println(responseBody)
 }
